6386184d1a58a3cbcc48d04d1698ad1db309c01b
Luke Bogdanovic <19232691@studentmail.ul.ie>
2022-04-18 02:50:58 +0100

    NO-JIRA Checklist Activity working

---
+++ app/src/main/java/com/ul/mobileappproject/ChecklistActivity.java
@@ -0,0 +1,77 @@
+package com.ul.mobileappproject;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ChecklistActivity extends AppCompatActivity {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView listViewItems;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checklist);
+        listViewItems = (ListView) findViewById(R.id.lvItems);
+        readItems();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        listViewItems.setAdapter(itemsAdapter);
+        setUpListViewListener();
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+
+    public void onAddItem(View view) {
+        EditText editTextNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = editTextNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        editTextNewItem.setText("");
+        writeItems();
+    }
+
+    private void setUpListViewListener() {
+        listViewItems.setOnItemLongClickListener((adapter, view, pos, id) -> {
+            items.remove(pos);
+            itemsAdapter.notifyDataSetChanged();
+            writeItems();
+            return true;
+        });
+    }
+
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+        } catch (IOException exception) {
+            items = new ArrayList<>();
+        }
+    }
+
+    private void writeItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+}
--- app/src/main/java/com/ul/mobileappproject/DashboardActivity.java
+++ app/src/main/java/com/ul/mobileappproject/DashboardActivity.java
@@ -1,14 +1,27 @@
 package com.ul.mobileappproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DashboardActivity extends AppCompatActivity {
 
+    private Button checkListBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        initializeUI();
+        checkListBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(DashboardActivity.this,ChecklistActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    private void initializeUI() {
+        checkListBtn = findViewById(R.id.checkListBtn);
     }
 }
