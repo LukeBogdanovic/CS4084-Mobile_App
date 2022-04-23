9991beaa516ddb4d0edcf5fc2d6d0a5b1915b9fa
jackconryan <58440324+jackconryan@users.noreply.github.com>
2022-04-24 00:45:23 +0100

    added search map for practically anything functionality

--- app/src/main/java/com/ul/mobileappproject/MapsActivity.java
+++ app/src/main/java/com/ul/mobileappproject/MapsActivity.java
@@ -1,34 +1,47 @@
 package com.ul.mobileappproject;
 
-import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
+
 import androidx.fragment.app.FragmentActivity;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
+import android.content.Intent;
 import android.annotation.SuppressLint;
+import android.location.Address;
+import android.location.Geocoder;
+import android.net.Uri;
 import android.os.Bundle;
 
 import android.Manifest.permission;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 import com.ul.mobileappproject.databinding.ActivityMapsBinding;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-//    request code for location permission request
+    EditText locSearch;
+    ImageView searchIcon;
 
-    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
-
-//     Flag indicating whether a requested permission has been denied after returning in {@link
-//     #onRequestPermissionsResult(int, String[], int[])}.
-
-    private boolean permissionDenied = false;
+    private boolean isPermissionGranted;
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
@@ -40,67 +53,92 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment =
-                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        locSearch = findViewById(R.id.search);
+        searchIcon = findViewById(R.id.search_icon);
+
+        searchIcon.setOnClickListener(this::geoLocate);
+        checkMyPermission();
+        if (isPermissionGranted) {
+            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+            SupportMapFragment mapFragment =
+                    (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+
+        }
+    }
+
+    private void geoLocate(View view) {
+        String locationName = locSearch.getText().toString();
+
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            List<Address> addressList = geocoder.getFromLocationName(locationName, 1);
+
+            if (addressList.size() > 0) {
+                Address address = addressList.get(0);
+
+                gotoLocation(address.getLatitude(), address.getLongitude());
+
+                mMap.addMarker(new MarkerOptions().position(new LatLng(address.getLatitude(), address.getLongitude())));
+
+                Toast.makeText(this, address.getLocality(), Toast.LENGTH_SHORT).show();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void gotoLocation(double latitude, double longitude) {
+        LatLng LatLng = new LatLng(latitude, longitude);
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(LatLng, 18);
+        mMap.moveCamera(cameraUpdate);
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
     }
 
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * we just add a marker near Stables.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        if (isPermissionGranted) {
+            mMap.setMyLocationEnabled(true);
+        }
         // Add a marker onto Stables and move the camera
         LatLng stables = new LatLng(52.6730436, -8.5709671);
         mMap.addMarker(new MarkerOptions().position(stables).title("Marker on Stables"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(stables));
     }
 
-//    /**
-//     * Enables the My Location layer if the fine location permission has been granted.
-//     */
-//    @SuppressLint("MissingPermission")
-//    private void enableMyLocation() {
-//        // 1. Check if permissions are granted, if so, enable the my location layer
-//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-//                == PackageManager.PERMISSION_GRANTED
-//                || ContextCompat.checkSelfPermission(this, permission.ACCESS_COARSE_LOCATION)
-//                == PackageManager.PERMISSION_GRANTED) {
-//            mMap.setMyLocationEnabled(true);
-//            return;
-//        }
-//
-//        // 2. Otherwise, request location permissions from the user.
-//        PermissionUtils.requestLocationPermissions(this, LOCATION_PERMISSION_REQUEST_CODE, true);
-//    }
-//
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-//                                           @NonNull int[] grantResults) {
-//        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
-//            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//            return;
-//        }
-//
-//        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
-//                Manifest.permission.ACCESS_FINE_LOCATION) || PermissionUtils
-//                .isPermissionGranted(permissions, grantResults,
-//                        Manifest.permission.ACCESS_COARSE_LOCATION)) {
-//            // Enable the my location layer if the permission has been granted.
-//            enableMyLocation();
-//        } else {
-//            // Permission was denied. Display an error message
-//            // Display the missing permission error dialog when the fragments resume.
-//            permissionDenied = true;
-//        }
-//    }
+    private void checkMyPermission() {
+        Dexter.withContext(this).withPermission(permission.ACCESS_FINE_LOCATION).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                Toast.makeText(MapsActivity.this, "permission Granted", Toast.LENGTH_SHORT).show();
+                isPermissionGranted = true;
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", getPackageName(), "");
+                intent.setData(uri);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+            }
+        }).check();
+    }
 }
